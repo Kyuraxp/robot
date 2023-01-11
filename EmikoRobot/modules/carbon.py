439ac53d96fd8557a2f82dc8e3056e2f8e97fcc0
Kyuraxp <96605016+Kyuraxp@users.noreply.github.com>
2023-01-11 20:21:38 +0700

    Update carbon.py

--- EmikoRobot/modules/carbon.py
+++ EmikoRobot/modules/carbon.py
@@ -24,15 +24,15 @@ async def carbon_func(_, message):
     carbon.close()
 
 
-MEMEK = "https://telegra.ph/file/11b5922a33de9968cedfe.jpg"
+MEMEK = "https://telegra.ph/file/a9f2c0b24ea825378b683.jpg"
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=MEMEK,
-        caption=f"""✨ **Hey I'm Emiko Robot** 
+        caption=f""" **Hey I'm Douglas Robot** 
 
-**Owner repo : [sena-ex](https://t.me/excrybaby)**
+**Owner repo : [Hisoka](https://t.me/hisoka69)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
@@ -44,9 +44,9 @@ async def repo(_, message):
             [
                 [
                     InlineKeyboardButton(
-                        "Repo", url="https://github.com/kennedy-ex/emikorobot"), 
+                        "Repo", url="https://www.xnxx.com"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/emikosupport")
+                        "Support", url="https://t.me/Liveshowbanget")
                 ]
             ]
         )
